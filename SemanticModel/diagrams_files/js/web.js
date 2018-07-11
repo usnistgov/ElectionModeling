ee945b27e5e2b349a0da66d15dbef61cd1c9c7b2
John Dziurlaj <john@hiltonroscoe.com>
2018-07-11 18:13:07 -0400

    update semantic model diagrams

--- SemanticModel/diagrams_files/js/web.js
+++ SemanticModel/diagrams_files/js/web.js
@@ -312,7 +312,7 @@ function createChild(responseXML) {
 								relationUL = parentUL.firstChild.lastChild;
 							else {
 								var relationLI = addNode(parentUL, 'Relations', 'javascript:void(0);',
-									'diagrams_files/icon_Relationship_1713546896.png', 'true');
+									'diagrams_files/icon_Relationship_235154585.png', 'true');
 								relationLI.setAttribute('refid', 'relations');
 								relationUL = document.createElement('ul');
 								relationUL.onExpand = function() {
