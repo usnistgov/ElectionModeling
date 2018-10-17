10d06ff754fb871f6dfb1f0d3e4b40c042a6b3a0
John Dziurlaj <john@hiltonroscoe.com>
2018-10-17 14:38:34 -0400

    update web reports

--- SemanticModel/diagrams_files/js/web.js
+++ SemanticModel/diagrams_files/js/web.js
@@ -313,7 +313,7 @@ function createChild(responseXML) {
 								relationUL = parentUL.firstChild.lastChild;
 							else {
 								var relationLI = addNode(parentUL, 'Relations', 'javascript:void(0);',
-									'diagrams_files/icon_Relationship_622772003.png', 'true');
+									'diagrams_files/icon_Relationship_2078517710.png', 'true');
 								relationLI.setAttribute('refid', 'relations');
 								relationUL = document.createElement('ul');
 								relationUL.onExpand = function() {
